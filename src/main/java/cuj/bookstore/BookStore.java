08329ddfb4df1c9669fbd509fe156c0e8d119cc9
cheben <cheben@qq.com>
2018-04-03 22:22:55 +0800

    项目创建，第一次提交，车奔，4月3日

---
+++ src/main/java/cuj/bookstore/BookStore.java
@@ -0,0 +1,10 @@
+package cuj.bookstore;
+
+/**
+ * Created by cujamin on 2018/4/3.
+ */
+public class BookStore {
+    public static void main(String[] args) {
+        System.out.println("i am a BOOKSTORE");
+    }
+}
